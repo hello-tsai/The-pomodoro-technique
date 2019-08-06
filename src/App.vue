f3259fc8d6a0ed079976639c1d1c67fa41765ae1
cai <xaioting.caicai@gmail.com>
2019-08-06 20:53:06 +0800

    作品完成

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+      <popup></popup>
+      <time-title></time-title>
+      <Time ></Time>
+  </div>
+</template>
+
+<script>
+import TimeTitle from "./components/TimeTitle";
+import Time from "./components/Time";
+import popup from "./components/common/popup";
+export default {
+   components:{
+     TimeTitle,
+      Time,
+       popup,
+   },
+    data(){
+       return{
+
+       }
+    },
+    methods:{
+        aaa(){
+            clearInterval(this.time)
+        },
+        bbb(){
+            clearInterval(this.time)
+        },
+    }
+}
+</script>
+
+<style>
+  @import "assets/styles/global.scss";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+}
+</style>
